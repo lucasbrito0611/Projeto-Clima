66a68e5c326ec31f3976a01e52e70769dfc76e6e
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-05-28 21:25:57 -0300

    Ajustando link

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const apiKey = 'b86a90b91f8e8d76d3fdb0b24fe39cea'
 
 export async function fetchCityId(cityName) {
     try {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&lang=pt`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&lang=pt`)
         
         if (!response.ok) {
             throw new Error(`Erro: ${response.status}. Please see https://openweathermap.org/faq#error${response.status} for more info.`)
