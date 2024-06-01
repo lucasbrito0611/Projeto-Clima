7c8115d0ee673d717c11ff4f5ed5989ff487915b
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-05-31 23:22:50 -0300

    Estilizando as informações

--- src/script.js
+++ src/script.js
@@ -2,6 +2,13 @@ import { fetchCityId, fetchCityData } from './api.js';
 
 const todayContainer = document.getElementById('todayContainer')
 const cityInput = document.getElementById('cityInput')
+const skyWeather = document.querySelector('#skyInfo p')
+const weatherIcon = document.getElementById('weatherIcon')
+const city = document.getElementById('city')
+const localTime = document.getElementById('localTime')
+const localDatetxt = document.getElementById('localDate')
+const lastUpdatetxt = document.getElementById('lastUpdate')
+
 
 let apiDataNow = null
 let apiDataForecast = null
@@ -21,7 +28,7 @@ async function catchApiData(cityName) {
     apiDataNow = dataNow
     apiDataForecast = dataForecast
 
-    showCityData()
+    apiData()
 }
 
 function formatHour(hour, timezone) {
@@ -29,39 +36,69 @@ function formatHour(hour, timezone) {
     return new Date(milliSeconds)
 }
 
-function showCityData() {
-    let temperature = apiDataNow.main.temp - 273.15
-    let sensation = apiDataNow.main.feels_like - 273.15
-    let hour = apiDataNow.dt
-    let cityTimezone = apiDataForecast.city.timezone
-    let formattedHour = formatHour(hour, cityTimezone).toLocaleTimeString('pt-BR', {hour: '2-digit', minute: '2-digit'})
-    let lastUpdate = new Date(apiDataNow.dt * 1000).toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
+// function showCityData() {
+//     let temperature = apiDataNow.main.temp - 273.15
+//     let sensation = apiDataNow.main.feels_like - 273.15
+//     let hour = apiDataNow.dt
+//     let cityTimezone = apiDataForecast.city.timezone
+//     let formattedHour = formatHour(hour, cityTimezone).toLocaleTimeString('pt-BR', {hour: '2-digit', minute: '2-digit'})
+//     let lastUpdate = new Date(apiDataNow.dt * 1000).toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
 
-    let forecast = apiDataForecast.list
+//     let forecast = apiDataForecast.list
     
-    // let forecastString = forecast.map(object => {
-    //     const date = new Date(object.dt_txt)
-    //     const formattedDate = date.toLocaleDateString('pt-BR') + ' ' + date.toLocaleTimeString('pt-BR')
+//     let forecastString = forecast.map(object => {
+//         const date = new Date(object.dt_txt)
+//         const formattedDate = date.toLocaleDateString('pt-BR') + ' ' + date.toLocaleTimeString('pt-BR')
 
-    //     const temperatureForecast = object.main.temp - 273.15
+//         const temperatureForecast = object.main.temp - 273.15
 
-    //     return `Data: ${formattedDate}, Temperatura: ${temperatureForecast.toFixed(1)}°C`
-    // }).join('<br>')
+//         return `Data: ${formattedDate}, Temperatura: ${temperatureForecast.toFixed(1)}°C`
+//     }).join('<br>')
 
-    todayContainer.innerHTML = `
-    Cidade: ${apiDataForecast.city.name}<br>
-    País: ${apiDataForecast.city.country}<br>
-    Horário: ${formattedHour}<br>
-    Última atualização: ${lastUpdate}<br>
-    Temperatura: ${Math.round(temperature)}°C<br>
-    Sensação Térmica: ${Math.round(sensation)}°C<br>`
+//     todayContainer.innerHTML = `
+//     Local: ${apiDataNow.name}, ${apiDataNow.sys.country}<br>
+    
+//     Horário: ${formattedHour}<br>
+//     Última atualização: ${lastUpdate}<br>
+//     Temperatura: ${Math.round(temperature)}°C<br>
+//     Sensação Térmica: ${Math.round(sensation)}°C<br>`
+// }
+
+function apiData() {
+    const skyNow = apiDataNow.weather[0].description
+    const icon = apiDataNow.weather[0].icon
+    const cityName = apiDataNow.name
+    const country = apiDataNow.sys.country
+
+    const lastUpdate = apiDataNow.dt
+    const cityTimezone = apiDataNow.timezone
+    const formattedHour = formatHour(lastUpdate, cityTimezone).toLocaleTimeString('pt-BR', {hour: '2-digit', minute: '2-digit'})
+    const localDate = formatHour(lastUpdate, cityTimezone).toLocaleDateString()
+    const formattedLastUpdate = new Date(lastUpdate * 1000).toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
+
+    todayInfo(skyNow, icon, cityName, country, formattedHour, localDate, formattedLastUpdate)
+}
+
+function todayInfo(skyNow, icon, cityName, country, formattedHour, localDate, formattedLastUpdate) {
+    skyWeather.textContent = skyNow.charAt(0).toUpperCase() + skyNow.slice(1)
+    weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+    city.textContent = `${cityName}, ${country}`
+    localTime.textContent = formattedHour
+    localDatetxt.textContent = localDate
+    lastUpdatetxt.textContent = formattedLastUpdate
+
 }
 
 function searchCity(event) {
     if (event.key === 'Enter') {
         event.preventDefault()
         catchApiData(cityInput.value.trim())
+        todayContainer.style.display = 'block'
+        
+        cityInput.value = ''
     }
 }
+
 
 cityInput.addEventListener('keydown', searchCity)
