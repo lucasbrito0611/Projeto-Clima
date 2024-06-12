6078f7b16264b61611b24c75a6a3eab638a93e51
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-06-11 22:16:03 -0300

    Finalizando o container da previsão de amanhã

--- src/script.js
+++ src/script.js
@@ -25,6 +25,12 @@ const tomorrowDate_txt = document.getElementById('tomorrowDate')
 const forecastTemp_txt = document.getElementById('forecastTemp')
 const forecastMax_txt = document.getElementById('forecastMax')
 const forecastMin_txt = document.getElementById('forecastMin')
+const forecastPrecipitation_txt = document.getElementById('forecastPrecipitation')
+const forecastHumidity_txt = document.getElementById('forecastHumidity')
+const forecastWind_txt = document.getElementById('forecastWind')
+const forecastSensation_txt = document.getElementById('forecastSensation')
+const forecastVisibility_txt = document.getElementById('forecastVisibility')
+const main_txt = document.getElementById('main')
 
 let apiDataNow = null
 let apiDataForecast = null
@@ -61,6 +67,20 @@ function formatTime(timezone) {
     return currentCityTime
 }
 
+function translate(text) {
+    const translations = {
+        'Clear': 'Limpo',
+        'Rain': 'Chuva',
+        'Clouds': 'Nuvens',
+    }
+
+    return translations[text] || text
+}
+
+function calcAverage(list) {
+    return list.reduce((sum, currentValue) => sum + currentValue, 0) / list.length
+}
+
 function apiData() {
     const skyNow = apiDataNow.weather[0].description
     const icon = apiDataNow.weather[0].icon
@@ -93,7 +113,7 @@ function apiData() {
     let maxTemp = apiDataNow.main.temp_max
 
     let forecastList = apiDataForecast.list
-    let avgPrecipitation = 0
+    let avgTodayPrecipitation = 0
 
     const forecastToday = []
     const forecastTomorrow = []
@@ -120,11 +140,48 @@ function apiData() {
         maxTemp = Math.max(...temperatures)
 
         const precipitations = forecastToday.map(forecast => forecast.pop)
-        avgPrecipitation = precipitations.reduce((previousValue, currenteValue) => previousValue + currenteValue, 0) / precipitations.length
+        avgTodayPrecipitation = calcAverage(precipitations)
     }
 
-    todayInfo(skyNow, icon, cityName, country, localDate, localTime, gmt, formattedLastUpdate, currentTemp, minTemp, maxTemp, avgPrecipitation, sensation, humidity, windSpeed, pressure, visibility, clouds, sunrise, sunset)
-    forecastInfo(tomorrowDate, forecastTomorrow)
+    const temperatures = forecastTomorrow.map(forecast => forecast.main.temp)
+    const forecastMaxTemp = Math.max(...temperatures)
+    const forecastMinTemp = Math.min(...temperatures)
+    const avgTemperature = calcAverage(temperatures)
+
+    const forecastPrecipitations = forecastTomorrow.map(forecast => forecast.pop)
+    const avgTomorrowPrecipitation = calcAverage(forecastPrecipitations)
+
+    const tomorrowMainList = forecastTomorrow.map(forecast => forecast.weather[0].main)
+
+    let frequency = {}
+    tomorrowMainList.forEach(main => {
+        if (frequency[main]) {
+            frequency[main]++
+        } else {
+            frequency[main] = 1
+        }
+    })
+
+    const oftenMain = Object.entries(frequency).reduce((max, value) => {
+        return value[1] > max[1] ? value : max
+    })
+    
+    const tomorrowMain = translate(oftenMain[0])
+    
+    const humidityList = forecastTomorrow.map(forecast => forecast.main.humidity)
+    const avgHumidity = calcAverage(humidityList)
+
+    const windList = forecastTomorrow.map(forecast => forecast.wind.speed)
+    const avgWind = calcAverage(windList)
+
+    const sensationList = forecastTomorrow.map(forecast => forecast.main.feels_like)
+    const avgSensation = calcAverage(sensationList)
+
+    const visibilityList = forecastTomorrow.map(forecast => forecast.visibility)
+    const avgVisibility = calcAverage(visibilityList)
+
+    todayInfo(skyNow, icon, cityName, country, localDate, localTime, gmt, formattedLastUpdate, currentTemp, minTemp, maxTemp, avgTodayPrecipitation, sensation, humidity, windSpeed, pressure, visibility, clouds, sunrise, sunset)
+    forecastInfo(tomorrowDate, tomorrowMain, avgTemperature, forecastMaxTemp, forecastMinTemp, avgTomorrowPrecipitation, avgHumidity, avgWind, avgSensation, avgVisibility)
 }
 
 function todayInfo(skyNow, icon, cityName, country, localDate, localTime, gmt, formattedLastUpdate, currentTemp, minTemp, maxTemp, avgPrecipitation, sensation, humidity, windSpeed, pressure, visibility, clouds, sunrise, sunset) {
@@ -153,14 +210,18 @@ function todayInfo(skyNow, icon, cityName, country, localDate, localTime, gmt, f
     sunset_txt.textContent = sunset.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit', timeZone: 'UTC'})
 }
 
-function forecastInfo(tomorrowDate, forecastTomorrow) {
-    const temperatures = forecastTomorrow.map(forecast => forecast.main.temp)
-    const avgTemperature = temperatures.reduce((previousValue, currenteValue) => previousValue + currenteValue, 0) / temperatures.length
-    
+function forecastInfo(tomorrowDate, tomorrowMain, avgTemperature, forecastMaxTemp, forecastMinTemp, avgTomorrowPrecipitation, avgHumidity, avgWind, avgSensation, avgVisibility) {
     tomorrowDate_txt.textContent = tomorrowDate.toLocaleDateString('pt-BR')
+    main_txt.textContent = tomorrowMain
+
     forecastTemp_txt.textContent = Math.round(avgTemperature - 273.15)
-    forecastMax_txt.textContent = Math.round(Math.max(...temperatures) - 273.15)
-    forecastMin_txt.textContent = Math.round(Math.min(...temperatures) - 273.15)
+    forecastMax_txt.textContent = Math.round(forecastMaxTemp - 273.15)
+    forecastMin_txt.textContent = Math.round(forecastMinTemp - 273.15)
+    forecastPrecipitation_txt.textContent = Math.round(avgTomorrowPrecipitation * 100)
+    forecastHumidity_txt.textContent = Math.round(avgHumidity)
+    forecastWind_txt.textContent = Math.round(avgWind * 3.6)
+    forecastSensation_txt.textContent = Math.round(avgSensation - 273.15)
+    forecastVisibility_txt.textContent = (((avgVisibility / 1000)).toFixed(1) * 1000) % 1000 === 0 ? (((avgVisibility / 1000)).toFixed(1) * 1000) / 1000 : (avgVisibility / 1000).toFixed(1).replace(/\./g, ',')
 }
 
 function searchCity(event) {
